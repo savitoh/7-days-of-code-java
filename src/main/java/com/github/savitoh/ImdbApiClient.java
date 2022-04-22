4f6250415823a405356152e8283a3af922cd1b85
Sávio Raires <savioraires10@gmail.com>
2022-04-22 05:06:29 -0300

    feat: refactor ImdbApiClient

--- src/main/java/com/github/savitoh/ImdbApiClient.java
+++ src/main/java/com/github/savitoh/ImdbApiClient.java
@@ -7,18 +7,14 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.Duration;
 
-public class ImdbClient {
-
-    private static final String IMDB_API = "https://imdb-api.com/en/API/Top250Movies/";
-    private static final String IMDB_API_KEY = System.getenv("IMDB_API_KEY");
-
+public class ImdbApiClient {
     private final HttpRequest httpRequest;
 
     private final HttpClient httpClient;
 
-    public ImdbClient() {
+    public ImdbApiClient(final String uri, final String apiKey) {
         this.httpRequest = HttpRequest.newBuilder()
-                .uri(URI.create(IMDB_API + IMDB_API_KEY))
+                .uri(URI.create(uri + apiKey))
                 .timeout(Duration.ofMinutes(1))
                 .header("Accept", "application/json")
                 .GET()
@@ -27,6 +23,7 @@ public class ImdbClient {
                 .connectTimeout(Duration.ofMinutes(1))
                 .build();
     }
+
     public String getTop250Movies() throws IOException, InterruptedException {
         HttpResponse<String> response = this.httpClient.send(this.httpRequest, HttpResponse.BodyHandlers.ofString());
         return response.body();
--- src/main/java/com/github/savitoh/Main.java
+++ src/main/java/com/github/savitoh/Main.java
@@ -6,8 +6,12 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        final ImdbClient imdbClient = new ImdbClient();
-        final String top2500Movies = imdbClient.getTop250Movies();
+        final String uri = "https://imdb-api.com/en/API/Top250Movies/";
+        final String apiKey = System.getenv("IMDB_API_KEY");
+        final ImdbApiClient imdbApiClient = new ImdbApiClient(uri, apiKey);
+
+
+        final String top2500Movies = imdbApiClient.getTop250Movies();
         final List<Movie> movies = new ParserJsonMovies(top2500Movies).parse();
         System.out.println("Top 250 Movies \n" + movies);
     }
