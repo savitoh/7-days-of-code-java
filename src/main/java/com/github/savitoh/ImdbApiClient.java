c51ca98adb3095ee660b8c725e68bfb92353d5e6
Sávio Raires <savioraires10@gmail.com>
2022-04-27 15:40:05 -0300

    feat: add Google's Style - checkstyle

--- src/main/java/com/github/savitoh/ImdbApiClient.java
+++ src/main/java/com/github/savitoh/ImdbApiClient.java
@@ -8,24 +8,26 @@ import java.net.http.HttpResponse;
 import java.time.Duration;
 
 public class ImdbApiClient {
-    private final HttpRequest httpRequest;
 
-    private final HttpClient httpClient;
+  private final HttpRequest httpRequest;
 
-    public ImdbApiClient(final String uri, final String apiKey) {
-        this.httpRequest = HttpRequest.newBuilder()
-                .uri(URI.create(uri + apiKey))
-                .timeout(Duration.ofMinutes(1))
-                .header("Accept", "application/json")
-                .GET()
-                .build();
-        this.httpClient = HttpClient.newBuilder()
-                .connectTimeout(Duration.ofMinutes(1))
-                .build();
-    }
+  private final HttpClient httpClient;
 
-    public String getTop250Movies() throws IOException, InterruptedException {
-        HttpResponse<String> response = this.httpClient.send(this.httpRequest, HttpResponse.BodyHandlers.ofString());
-        return response.body();
-    }
+  public ImdbApiClient(final String uri, final String apiKey) {
+    this.httpRequest = HttpRequest.newBuilder()
+        .uri(URI.create(uri + apiKey))
+        .timeout(Duration.ofMinutes(1))
+        .header("Accept", "application/json")
+        .GET()
+        .build();
+    this.httpClient = HttpClient.newBuilder()
+        .connectTimeout(Duration.ofMinutes(1))
+        .build();
+  }
+
+  public String getTop250Movies() throws IOException, InterruptedException {
+    HttpResponse<String> response = this.httpClient.send(this.httpRequest,
+        HttpResponse.BodyHandlers.ofString());
+    return response.body();
+  }
 }
--- src/test/java/com/github/savitoh/ParserJsonMoviesTest.java
+++ src/test/java/com/github/savitoh/ParserJsonMoviesTest.java
@@ -1,106 +1,106 @@
 package com.github.savitoh;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+import java.util.List;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.List;
-import java.util.stream.Stream;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.params.provider.Arguments.arguments;
-
 class ParserJsonMoviesTest {
 
 
-    private static final String jsonMovie = """
-            {
-                "items": [
-                    {
-                        "id": "tt0111161",
-                        "rank": "1",
-                        "title": "The Shawshank Redemption",
-                        "fullTitle": "The Shawshank Redemption (1994)",
-                        "year": "1994",
-                        "image": "https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX128_CR0,3,128,176_AL_.jpg",
-                        "crew": "Frank Darabont (dir.), Tim Robbins, Morgan Freeman",
-                        "imDbRating": "9.2",
-                        "imDbRatingCount": "2574700"
-                    }
-                ],
-                "errorMessage": ""
-            }""".stripIndent();
-    private static final String jsonTwoMovies = """
-            {
-                "items": [
-                    {
-                        "id": "tt0111161",
-                        "rank": "1",
-                        "title": "The Shawshank Redemption",
-                        "fullTitle": "The Shawshank Redemption (1994)",
-                        "year": "1994",
-                        "image": "https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX128_CR0,3,128,176_AL_.jpg",
-                        "crew": "Frank Darabont (dir.), Tim Robbins, Morgan Freeman",
-                        "imDbRating": "9.2",
-                        "imDbRatingCount": "2574700"
-                    },
-                    {
-                        "id": "tt0068646",
-                        "rank": "2",
-                        "title": "The Godfather",
-                        "fullTitle": "The Godfather (1972)",
-                        "year": "1972",
-                        "image": "https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UX128_CR0,1,128,176_AL_.jpg",
-                        "crew": "Francis Ford Coppola (dir.), Marlon Brando, Al Pacino",
-                        "imDbRating": "9.2",
-                        "imDbRatingCount": "1772902"
-                    }
-                ],
-                "errorMessage": ""
-            }""".stripIndent();
+  private static final String jsonMovie = """
+      {
+          "items": [
+              {
+                  "id": "tt0111161",
+                  "rank": "1",
+                  "title": "The Shawshank Redemption",
+                  "fullTitle": "The Shawshank Redemption (1994)",
+                  "year": "1994",
+                  "image": "https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX128_CR0,3,128,176_AL_.jpg",
+                  "crew": "Frank Darabont (dir.), Tim Robbins, Morgan Freeman",
+                  "imDbRating": "9.2",
+                  "imDbRatingCount": "2574700"
+              }
+          ],
+          "errorMessage": ""
+      }""".stripIndent();
+  private static final String jsonTwoMovies = """
+      {
+          "items": [
+              {
+                  "id": "tt0111161",
+                  "rank": "1",
+                  "title": "The Shawshank Redemption",
+                  "fullTitle": "The Shawshank Redemption (1994)",
+                  "year": "1994",
+                  "image": "https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX128_CR0,3,128,176_AL_.jpg",
+                  "crew": "Frank Darabont (dir.), Tim Robbins, Morgan Freeman",
+                  "imDbRating": "9.2",
+                  "imDbRatingCount": "2574700"
+              },
+              {
+                  "id": "tt0068646",
+                  "rank": "2",
+                  "title": "The Godfather",
+                  "fullTitle": "The Godfather (1972)",
+                  "year": "1972",
+                  "image": "https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UX128_CR0,1,128,176_AL_.jpg",
+                  "crew": "Francis Ford Coppola (dir.), Marlon Brando, Al Pacino",
+                  "imDbRating": "9.2",
+                  "imDbRatingCount": "1772902"
+              }
+          ],
+          "errorMessage": ""
+      }""".stripIndent();
 
+  public static Stream<Arguments> jsonMapMovies() {
+    return Stream.of(
+        arguments(jsonMovie, List.of(new Movie("The Shawshank Redemption",
+            "https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX128_CR0,3,128,176_AL_.jpg",
+            9.2,
+            1994))
+        ),
+        arguments(jsonTwoMovies, List.of(new Movie("The Shawshank Redemption",
+                "https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX128_CR0,3,128,176_AL_.jpg",
+                9.2,
+                1994),
+            new Movie("The Godfather",
+                "https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UX128_CR0,1,128,176_AL_.jpg",
+                9.2,
+                1972))
+        )
+    );
+  }
 
-    @Test
-    void Should_ThrowNPE_When_InitializeWithJsonNull() {
-        assertThrows(NullPointerException.class, () -> new ParserJsonMovies(null), "'json' cannot be null.");
-    }
+  @Test
+  void Should_ThrowNPE_When_InitializeWithJsonNull() {
+    assertThrows(NullPointerException.class, () -> new ParserJsonMovies(null),
+        "'json' cannot be null.");
+  }
 
-    @Test
-    @Disabled("Waiting for: valid json movie format")
-    void Should_ThrowIllegalArgumentException_When_InitializeWithJsonNull() {
-        assertThrows(IllegalArgumentException.class, () -> new ParserJsonMovies("{}"), "'json' does not follow structure expected.");
-    }
+  @Test
+  @Disabled("Waiting for: valid json movie format")
+  void Should_ThrowIllegalArgumentException_When_InitializeWithJsonNull() {
+    assertThrows(IllegalArgumentException.class, () -> new ParserJsonMovies("{}"),
+        "'json' does not follow structure expected.");
+  }
 
-    @ParameterizedTest
-    @MethodSource("jsonMapMovies")
-    void Should_ParseMovies_When_JsonMovieIsValid(String jsonMovie, List<Movie> moviesExpected) {
-        final ParserJsonMovies parserJsonMovies = new ParserJsonMovies(jsonMovie);
+  @ParameterizedTest
+  @MethodSource("jsonMapMovies")
+  void Should_ParseMovies_When_JsonMovieIsValid(String jsonMovie, List<Movie> moviesExpected) {
+    final ParserJsonMovies parserJsonMovies = new ParserJsonMovies(jsonMovie);
 
-        final List<Movie> movies = parserJsonMovies.parse();
+    final List<Movie> movies = parserJsonMovies.parse();
 
-        assertEquals(moviesExpected, movies);
-    }
-
-    public static Stream<Arguments> jsonMapMovies() {
-        return Stream.of(
-                arguments(jsonMovie, List.of(new Movie("The Shawshank Redemption",
-                        "https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX128_CR0,3,128,176_AL_.jpg",
-                        9.2,
-                        1994))
-                ),
-                arguments(jsonTwoMovies, List.of(new Movie("The Shawshank Redemption",
-                                "https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX128_CR0,3,128,176_AL_.jpg",
-                                9.2,
-                                1994),
-                        new Movie("The Godfather",
-                                "https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UX128_CR0,1,128,176_AL_.jpg",
-                                9.2,
-                                1972))
-                )
-        );
-    }
+    assertEquals(moviesExpected, movies);
+  }
 
 }
