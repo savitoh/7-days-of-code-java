c04b04848b2d748d8513112274795ed75a70ab7a
Sávio Raires <savioraires10@gmail.com>
2022-05-14 18:58:46 -0300

    refactor: rename classes and objects references

--- src/main/java/com/github/savitoh/Main.java
+++ src/main/java/com/github/savitoh/Main.java
@@ -1,5 +1,6 @@
 package com.github.savitoh;
 
+import com.github.savitoh.http.imdb.ImdbApiClient;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
@@ -7,12 +8,12 @@ import java.util.List;
 public class Main {
 
   public static void main(String[] args) throws IOException, InterruptedException {
-    final String uri = "https://imdb-api.com/en/API/Top250Movies/";
-    final String apiKey = System.getenv("IMDB_API_KEY");
-    final ImdbApiClient imdbApiClient = new ImdbApiClient(uri, apiKey);
+    final String imdbTop250MoviesUri = "https://imdb-api.com/en/API/Top250Movies/";
+    final String imdbApiKey = System.getenv("IMDB_API_KEY");
+    final ImdbApiClient imdbApiClient = new ImdbApiClient(imdbTop250MoviesUri, imdbApiKey);
 
     final String top250MoviesJson = imdbApiClient.getBody();
-    final List<Movie> movies = new ParserJsonMovies(top250MoviesJson).parse();
+    final List<Movie> movies = new ImdbMoviesJsonParser(top250MoviesJson).parse();
     try (final var writer = new FileWriter("movies.html")) {
       HtmlGenerator htmlGenerator = new HtmlGenerator(writer);
       htmlGenerator.generate(movies);
