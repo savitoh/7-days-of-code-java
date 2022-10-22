9755be4a0e5faa2db3dedf1470a7c7fa639dccb6
Sávio Raires de Souza <savioraires10@gmail.com>
2022-10-22 14:16:47 -0300

    refactor: simplify IMDB Json Parser

--- src/main/java/com/github/savitoh/imdb/ImdbMoviesJsonParser.java
+++ src/main/java/com/github/savitoh/imdb/ImdbMoviesJsonParser.java
@@ -4,11 +4,10 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import com.github.savitoh.JsonParser;
+
 import java.util.List;
 import java.util.Objects;
-import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -17,9 +16,7 @@ public class ImdbMoviesJsonParser implements JsonParser {
   private static final Pattern MOVIE_PATTERN =
       Pattern.compile("\"items\"\\s*:\\s*(\\[.*?\\]),", Pattern.DOTALL);
   private final ObjectMapper mapper =
-      new ObjectMapper()
-          .enable(SerializationFeature.INDENT_OUTPUT)
-          .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+      new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
   private final String jsonMovies;
 
   public ImdbMoviesJsonParser(final String json) {
@@ -27,24 +24,19 @@ public class ImdbMoviesJsonParser implements JsonParser {
     this.jsonMovies = json;
   }
 
-  private Optional<String> extractByMatcher(final Matcher matcher) {
-    if (matcher.find()) {
-      return Optional.of(matcher.group(1));
-    }
-    return Optional.empty();
-  }
-
   private String extractMovies() {
-    return extractByMatcher(MOVIE_PATTERN.matcher(this.jsonMovies))
-        .orElseThrow(
-            () ->
-                new IllegalArgumentException(
-                    "Does not possible extract 'movies'. Json passed:\n" + jsonMovies));
+    Matcher matcher = MOVIE_PATTERN.matcher(this.jsonMovies);
+    if (matcher.find()) {
+      return matcher.group(1);
+    } else {
+      throw new IllegalArgumentException(
+          "Does not possible extract 'movies'. Json passed:\n" + this.jsonMovies);
+    }
   }
 
   @Override
   public List<Movie> parse() throws JsonProcessingException {
     final var movies = extractMovies();
-    return mapper.readValue(movies, new TypeReference<List<Movie>>() {});
+    return mapper.readValue(movies, new TypeReference<>() {});
   }
 }
